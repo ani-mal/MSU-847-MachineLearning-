23ab381d84fd10240f8efb033762a9ce59844598
Anna Paula Maule <a.pawlickamaule@techsmith.com>
2019-05-03 22:48:22 -0400

    squash

--- Homework 6/hardimpute.m
+++ Homework 6/hardimpute.m
@@ -4,12 +4,12 @@ function [X_complete] = hardimpute(X_missing, Omega, r)
 % Omega -- a m-by-n binary matrix, indicating location of the missing values
 % r -- rank
 
-old = zeros(size(X_missing));
+old = zeros(size(X_missing))
 for i=1:r
-    old(Omega)=0;
-    X = X_missing+old;
-    [U,S,V] = svd(X);
-    new = U(:,1:i)*S(1:i,1:i)*V(:,1:i)';
-    old = new;
+    old(Omega)=0
+    X = X_missing+old
+    [U,S,V] = svd(X)
+    new = U(:,1:i)*S(1:i,1:i)*V(:,1:i)'
+    old = new
 end
-X_complete = new;
+X_complete = new
